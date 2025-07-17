6cde20a809f6c6874a0897a47b16b3e29d21e0df
xeimun <hinote444@naver.com>
2025-07-17 20:52:24 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/com/example/helloworld/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.helloworld.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 API
+                .allowedOriginPatterns("*")  // 모든 주소 허용 (운영 환경에서는 사용 지양)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
